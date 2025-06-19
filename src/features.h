7c70bb9e0dd06e43380471e058deca8b6077e739
Xaverie Anne-Marie <xaverieanne.mballaami@etu.estia.fr>
2025-06-19 03:04:36 +0200

    recommit

--- src/features.h
+++ src/features.h
@@ -26,7 +26,6 @@ void min_component(char component, char *source_path, FILE* out);
 
 void stat_report(const char* FileImage);
 
-/*int write_image_data(const char *filename, unsigned char *data, int width, int height);*/
 void color_gray(const char* filename);
 
 void color_invert(char *source_path);
@@ -43,5 +42,6 @@ void color_blue (char *source_path);
 
 void color_red (char *source_path);
 
+void mirrorHorizontal (const char *fileImage_input, const char *fileImage_ouput);
 
 #endif
