1214cedbfd077e05cc896f561b41a511e3a67b56
OceaneParaik <oceane.paraik@etu.estia.fr>
2025-06-19 15:16:30 +0200

    modifs

--- src/main.c
+++ src/main.c
@@ -75,8 +75,7 @@ int main(int argc, char **argv) {
   }else if (strncmp(configuration.command, "color_red", 9) == 0) {
     color_red(configuration.filenames[0]);
   }else if (strncmp(configuration.command, "color_desaturate", 15)==0){
-    color_desaturate(configuration.filenames[0]);
-    
+    color_desaturate(configuration.filenames[0]);  
   }
   /*
    * TO COMPLETE
